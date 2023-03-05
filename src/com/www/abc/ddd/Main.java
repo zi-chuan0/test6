d5e7052e819308c462a10bbbc4745c8f2002a2df
zichuan <1684303778@qq.com>
2023-03-05 10:15:17 +0800

    first commit

---
+++ src/com/www/abc/ddd/Main.java
@@ -0,0 +1,23 @@
+package com.www.abc.ddd;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../sample/ggg/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/www/abc/sample/Controller.java
@@ -0,0 +1,4 @@
+package com.www.abc.sample;
+
+public class Controller {
+}
